68773e908b6511ad086f81c10846146774e5a67f
Jaime <jaimemp97@outlook.es>
2023-05-29 17:38:19 +0200

    Solution to problem 14

---
+++ python/easy/14_longest_common_preffix.py
@@ -0,0 +1,55 @@
+"""
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+"""
+
+
+class SuboptimalSolution(object):
+    def longestCommonPrefix(self, str_list):
+        """
+        :type str_list: List[str]
+        :rtype: str
+        """
+
+        if not str_list:
+            return ""
+
+        common_prefix = min(str_list, key=len)
+
+        while len(common_prefix) > 0:
+            for string in str_list:
+                if not string.startswith(common_prefix):
+                    common_prefix = common_prefix[:len(common_prefix) - 1]
+                    continue
+
+                if all(s.startswith(common_prefix) for s in str_list):
+                    return common_prefix
+
+        return ""
+
+
+class Solution(object):
+    def longestCommonPrefix(self, str_list: list[str]):
+        """
+        :type str_list: List[str]
+        :rtype: str
+        """
+
+        if not str_list:
+            return ""
+
+        common_prefix = min(str_list, key=len)
+
+        for i in range(len(common_prefix)):
+            if any(string[i] != common_prefix[i] for string in str_list):
+                return common_prefix[:i]
+
+        return common_prefix
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    strs = ["a"]
+    s = solution.longestCommonPrefix(strs)
+    print(s)
